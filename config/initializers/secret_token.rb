02ba824a5742bb02f1a0ebb28947f2817919ee10
Jorge Paez <george.paez@gmail.com>
2011-08-01 10:28:54 -0500

    Creacion proyecto un_centavo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UnCentavo::Application.config.secret_token = 'd15f912fb0fe3feedf95aadb7ec25c32894dfed08e6f331b228e96a241666a8ab61ef80244e284e3edace67670dc9995f0698b7a3aa5debf590cc240b189c958'
