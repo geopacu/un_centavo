3d90db6836e1e4c9b066589ecadd18c31f49d97f
Jorge Paez <george.paez@gmail.com>
2011-08-01 16:06:46 -0500

    Mantenedor Categoria

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 UnCentavo::Application.routes.draw do
+  resources :categorias
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
---
+++ db/migrate/20110801210205_create_categorias.rb
@@ -0,0 +1,14 @@
+class CreateCategorias < ActiveRecord::Migration
+  def self.up
+    create_table :categorias do |t|
+      t.string :nombre
+      t.string :descripcion
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :categorias
+  end
+end
